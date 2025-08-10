978aac1dfaf078452a4bd28794d7b783a2f3d50b
Артём <90091939+vo0ov@users.noreply.github.com>
2025-08-10 11:28:02 +0000

    Удалён старый пакет PyroArgs

--- PyroArgs/__init__.py
+++
@@ -1,8 +0,0 @@
-from . import errors, types
-from .core.base import BaseModule
-from .core.commands import command
-from .core.loader import ModuleLoader
-from .pyroargs import PyroArgs
-
-__all__ = ['PyroArgs', 'types', 'errors', 'ModuleLoader', 'BaseModule', 'command']
-__version__ = '1.5'  # ВЕРСИЯ
